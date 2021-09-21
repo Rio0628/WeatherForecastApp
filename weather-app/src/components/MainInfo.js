ce8ca572c18865a6ae0f2a6508e4d2c8eb75dadd
Rio0628 <mariomdomenech@gmail.com>
2021-09-20 23:48:34 -0400

    Initial Body and Components Added
    Main components added to the app. Groundwork being set in.

---
+++ weather-app/src/components/MainInfo.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class MainInfo extends React.Component {
+    render () {
+        return (
+            <div className='mainInfoContainer'>
+                
+            </div>
+        )
+    }
+}
+
+export default MainInfo
---
+++ weather-app/src/components/Searchbar.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Searchbar = () => {
+    return (
+        <input className='searchbar' type='text' placeholder='Search Zone'></input>
+    )
+}
+
+export default Searchbar
