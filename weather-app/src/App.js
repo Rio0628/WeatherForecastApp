97d625df2c6b4de6f718bec99fbbc25b09aa709e
Rio0628 <mariomdomenech@gmail.com>
2021-09-28 23:47:03 -0400

    Implemented the future weather API call
    - Complete the fetch API function for the future weather and stored it within an object to be used later on state.
    - Created function to get the day for  a certain weather variable by turning the raw data into a date and getting the first word from the resulting string.

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -10,6 +10,7 @@ class App extends React.Component {
     super(props)
     // Temporary State to populate component before inputting information of Weather API
     this.state = {
+      SeachWeatherStatus: false,
       SearchOnChange: '',
       ExampleForecast: [],
       CurrentForecast: 
@@ -27,7 +28,8 @@ class App extends React.Component {
     // console.log(api.openweathermap.org/data/2.5/weather?q=London&appid=f0caa45808a9789d4f46776484b799e2);
     let object;
     let ftrWthrObject;
-    function transferData(a, b, c, d, e, f) {
+    
+    const transferData = (a, b, c, d, e, f) => {
       object = {
         name: `${a}, ${d}`,
         wthr: b,
@@ -36,28 +38,48 @@ class App extends React.Component {
         lat: f
       }
     }
+
+    const transferFtrWthrData = (a, b, c, d, e) => {
+      ftrWthrObject = {
+        wthrDay1: a,
+        wthrDay2: b,
+        wthrDay3: c,
+        wthrDay4: d,
+        wthrDay5: e,
+      }
+      
+      ftrWthrObject.wthrDay1.dt = getFirstWordDate(ftrWthrObject.wthrDay1.dt);
+      ftrWthrObject.wthrDay2.dt = getFirstWordDate(ftrWthrObject.wthrDay2.dt);
+      ftrWthrObject.wthrDay3.dt = getFirstWordDate(ftrWthrObject.wthrDay3.dt);
+      ftrWthrObject.wthrDay4.dt = getFirstWordDate(ftrWthrObject.wthrDay4.dt);
+      ftrWthrObject.wthrDay5.dt = getFirstWordDate(ftrWthrObject.wthrDay5.dt);    
+    }
+
+    const getFirstWordDate = (wthrDay) => {
+      const dayDate = new Date(wthrDay * 1000).toString();
+      const firstWord = dayDate.split(" ")[0];
+
+      return firstWord;
+    }
     
     const dailyWeather = async (cityName) => {
       const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=f0caa45808a9789d4f46776484b799e2&units=metric`;
       let a = [];
 
       await axios.get(url).then(data => a.push(data.data))
-      
       transferData(a[0].name, a[0].weather[0].main, a[0].main.temp, a[0].sys.country, a[0].coord.lon, a[0].coord.lat)
       // .then(resp => resp.json()).then(data => transferData(data)).catch(err => console.log(err));
     } 
 
-    const futureWeather = async (cityName) => {
-      const url = `https://api.openweathermap.org/data/2.5/onecall?lat=25.7743&lon=-80.1937&exclude=current,minutely,hourly,alerts&units=metric&appid=4439be80c1f0ade164109e2399a51173
+    const futureWeather = async (lon, lat) => {
+      const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=current,minutely,hourly,alerts&units=metric&appid=4439be80c1f0ade164109e2399a51173
       `;
       // 4439be80c1f0ade164109e2399a51173
       let a = [];
     
-      await axios.get(url).then(data => a.push(data.data))
-      console.log(a)
+      await axios.get(url).then(data => a.push(data.data));
+      transferFtrWthrData(a[0].daily[0], a[0].daily[1], a[0].daily[2], a[0].daily[3], a[0].daily[4]);
     }
-
-    futureWeather('miami');
 
     const onChange = (e) => {
       console.log(e.target.value);
@@ -69,14 +91,18 @@ class App extends React.Component {
     }
 
     const chgMainState = (a) => this.setState({CurrentForecast: a});
+
+    const chgFtrWthrState = () => {};
     
     const onClick = async (e) => {
       console.log(e.target)
       if (e.target.className === 'submitBtn') {
         // console.log(this.state.SearchOnChange);
         await dailyWeather(this.state.SearchOnChange);
+        await futureWeather(object.long, object.lat);
         chgMainState(object)
         console.log(object)
+        console.log(ftrWthrObject);
         // document.body.style.background = color; == Use something like this in the future to change the background according to the weather type of the region.
       }
     }
@@ -93,7 +119,8 @@ class App extends React.Component {
             <li className='submitBtn' onClick={onClick}>Search</li>
           </div>
           
-
+          {/* Setup small arrow function to give the weather when the submit button is clicked so that the program does not have to render a said region before the user decides to search for function*/}
+          
           <MainInfo 
             name={this.state.CurrentForecast.name} 
             weather={this.state.CurrentForecast.wthr} 
